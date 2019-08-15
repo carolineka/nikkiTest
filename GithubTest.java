f1f8d34b5e5ebec7b6fab4997ff93c3a27680c0b
niklasbae <niklasbae@hotmail.com>
2019-08-15 15:17:22 +0200

    Nikki test

--- GithubTest.java
+++ GithubTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package githubtest;
 
 /**
  *
@@ -16,6 +10,8 @@ public class GithubTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        System.out.println("HELLO");
     }
     
 }
